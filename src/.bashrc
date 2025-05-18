e0a4a5bc46e0a448bc03564f4fc303e70d67635d
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-05-18 15:04:33 -0300

    organization and a new way to handle symlinks to prevent issues with recursive loops of symlinking

--- src/.bashrc
+++ src/.bashrc
@@ -118,3 +118,4 @@ fi
 . "$HOME/.cargo/env"
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
---
+++ src/i3wm/i3/power.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+chosen=$(echo -e "Shutdown\nReboot\nSuspend\nLock\nCancel" | rofi -dmenu -i -p "Power State")
+
+case "$chosen" in
+  *Poweroff) systemctl poweroff ;;
+  *Reboot) systemctl reboot ;;
+  *Suspend) systemctl suspend ;;
+  *Lock) i3lock ;;
+  *Cancel) exit 0 ;;
+esac
+z
