49c4e22a9e63677c2cce700acde5caaa5cbf4e64
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-05-23 05:57:24 -0300

    add my path env

--- src/fish/config.fish
+++ src/fish/config.fish
@@ -5,12 +5,30 @@ end
 # Rust environment
 set -gx PATH $HOME/.cargo/bin $PATH
 
-# Homebrew environment
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# Add ~/workspace/bin to path
+set -gx PATH $HOME/workspace/bin $PATH
 
-# ASDF path setup
-set -gx PATH (string replace -r '^$' "$HOME/.asdf" $ASDF_DATA_DIR)/shims $PATH
+# Homebrew environment
+# eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 # Personal aliases
 alias sizeof='du -h -d 1 | sort -hr'
 alias update='sudo apt update -y && sudo apt upgrade -y'
+
+if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+  exec startx
+fi
