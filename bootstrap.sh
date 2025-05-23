fdbc406be7bf08e82bb3d5f564d46e2f5e4b43a5
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-05-23 05:56:52 -0300

    add installation of essential pakcages in arch

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ ln -sf "$HOME/dotfiles/src/.bashrc" "$HOME/"
 ln -sf "$HOME/dotfiles/src/.bash_logout" "$HOME/"
 ln -sf "$HOME/dotfiles/src/.gitconfig" "$HOME/"
 ln -sf "$HOME/dotfiles/src/fish" "$HOME/.config/"
+ln -sf "$HOME/dotfiles/src/kitty" "$HOME/.config/"
 
 # i3wm config
 ln -sf "$HOME/dotfiles/src/i3wm/dconf" "$HOME/.config/"
@@ -20,3 +21,14 @@ ln -sf "$HOME/dotfiles/src/bleachbit" "$HOME/.config/"
 ln -sf "$HOME/dotfiles/src/VirtualBox" "$HOME/.config/"
 ln -sf "$HOME/dotfiles/src/parcellite" "$HOME/.config/"
 ln -sf "$HOME/dotfiles/src/neofetch" "$HOME/.config/"
+
+# install
+sudo pacman -S rofi git xdg-user-dirs ufw pulseaudio pulseaudio-alsa alsa-utils pavucontrol gnome-keyring seahorse breeze breeze-gtk breeze-icons qt5ct lxappearance 
+sudo pacman -S kvantum kvantum-theme-materia materia-gtk-theme capitaine-cursors arc-icon-theme lxappearance
+sudo pacman -S ttf-cascadia-code-nerd code 
+# firewall config
+ufw default deny && ufw allow from 192.168.0.0/24 && ufw limit ssh
+ufw enable
+systemctl enable ufw
+systemctl start ufw
+ufw status
