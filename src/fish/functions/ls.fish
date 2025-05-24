6fe4e89b37771106a0e3097fedbb2b6fcf06c23a
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-05-24 19:49:51 -0300

    ls enhanced with eza

---
+++ src/fish/functions/ls.fish
@@ -0,0 +1,4 @@
+function ls --wraps='eza --icons always --group-directories-first -1' --description 'alias ls=eza --icons always --group-directories-first -1'
+  eza --icons always --group-directories-first -1 $argv
+        
+end
